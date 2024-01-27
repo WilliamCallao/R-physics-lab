f34d05185648b6ee59edbea7917d2b231c4eaf8c
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-01-27 18:17:24 -0400

    case 2

---
+++ Direct Current Sources/case2.R
@@ -0,0 +1,5 @@
+############### Case 2: V = 12 ###############
+
+analyze_case(V = 12, R = 1, ri_values = R_int, case_name = "2.1")
+analyze_case(V = 12, R = 10, ri_values = R_int, case_name = "2.2")
+analyze_case(V = 12, R = 50, ri_values = R_int, case_name = "2.3")
