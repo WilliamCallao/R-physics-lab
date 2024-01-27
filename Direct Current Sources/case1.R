119f57770cd84740516a6881f00700c4800fc0cd
WilliamCallao <96638909+WilliamCallao@users.noreply.github.com>
2024-01-27 18:16:19 -0400

    case 1

---
+++ Direct Current Sources/case1.R
@@ -0,0 +1,5 @@
+############### Case 1: V = 50 ###############
+
+analyze_case(V = 50, R = 12, ri_values = R_int, case_name = "1.1")
+analyze_case(V = 50, R = 43, ri_values = R_int, case_name = "1.2")
+analyze_case(V = 50, R = 81, ri_values = R_int, case_name = "1.3")
